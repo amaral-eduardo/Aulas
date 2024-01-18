ad4c850c98de2b2756e396aedf951d0bf83783e0
Eduardo <eduama15@gmail.com>
2024-01-17 21:23:46 -0300

    update

--- AnaliseSobrevivencia/Trab1/Trab1.R
+++ AnaliseSobrevivencia/Trab1/Trab1.R
@@ -28,14 +28,61 @@ Hemato <- subset(dados, gptumor=="Hemato", select=c('tempo', 'status'))
 dados1 <- dados[,c('tempo', 'status', 'gptumor')]
 
 # Analise descritiva ------------------------------------------------------
+
 boxplot(dados$idade ~ dados$gptumor)
 
 # Modelos parametricos ----------------------------------------------------
+require(survival)
+dados_sob <- Surv(dados1$tempo, dados1$status)
+
+km1 <- survfit(dados_sob~1, conf.type='plain')
+summary(km1)
+
+plot(km1,  mark.time=T, conf.int=T, main = "Completo")
+
+#### Loco
+Loco_sob <- Surv(Loco$tempo, Loco$status)
+km2 <- survfit(Loco_sob~1, conf.type='plain')
+
+summary(km2)
+
+plot(km2,  mark.time=T, conf.int=T, main = "Loco")
+
+km <- survfit(Loco_sob~1)
+na <- survfit(Loco_sob~1, type='fh')
+plot(km, mark.time=T, conf.int=F, lwd=2, xlab='Tempo de sobrevida', ylab='Prob. de sobrevida estimada')
+lines(na, mark.time=T, conf.int=F, lwd=2, col=2)
+legend(15,0.3, paste(c('Kaplan-Meier', 'Nelson-Aalen')), lwd=2, col=1:2, bty='o')
 
 
+#### Mtx
+Mtx_sob <- Surv(Mtx$tempo, Mtx$status)
+km3 <- survfit(Mtx_sob~1, conf.type='plain')
 
+summary(km3)
+
+plot(km3,  mark.time=T, conf.int=T, main = "Mtx")
+
+km <- survfit(Mtx_sob~1)
+na <- survfit(Mtx_sob~1, type='fh')
+plot(km, mark.time=T, conf.int=F, lwd=2, xlab='Tempo de sobrevida', ylab='Prob. de sobrevida estimada')
+lines(na, mark.time=T, conf.int=F, lwd=2, col=2)
+legend(150,0.8, paste(c('Kaplan-Meier', 'Nelson-Aalen')), lwd=2, col=1:2, bty='o')
+
+#### Hemato
+Hemato_sob <- Surv(Hemato$tempo, Hemato$status)
+km4 <- survfit(Hemato_sob~1, conf.type='plain')
+
+summary(km4)
+
+plot(km4,  mark.time=T, conf.int=T, main = "Hemato")
+
+km <- survfit(Hemato_sob~1)
+na <- survfit(Hemato_sob~1, type='fh')
+plot(km, mark.time=T, conf.int=F, lwd=2, xlab='Tempo de sobrevida', ylab='Prob. de sobrevida estimada')
+lines(na, mark.time=T, conf.int=F, lwd=2, col=2)
+legend(150,0.8, paste(c('Kaplan-Meier', 'Nelson-Aalen')), lwd=2, col=1:2, bty='o')
 # Selecao do modelo -------------------------------------------------------
-
 
 
 # Interpretacao ----------------------------------------------------------
