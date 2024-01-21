dffb934786f3a5cb811448ced9a6ce90310da99b
Eduardo Amaral <eduama15@gmail.com>
2024-01-21 10:21:42 -0300

    update

--- AnaliseSobrevivencia/Trab1/Trab1.R
+++ AnaliseSobrevivencia/Trab1/Trab1.R
@@ -36,6 +36,9 @@ Hemato_sob <- Surv(Hemato$tempo, Hemato$status)
 
 boxplot(dados$idade ~ dados$gptumor)
 
+
+# Funcao de sobrevivencia -------------------------------------------------
+
 require(survival)
 # dados_sob <- Surv(dados1$tempo, dados1$status)
 # 
@@ -87,8 +90,6 @@ require(survival)
 # lines(na, mark.time=T, conf.int=F, lwd=2, col=2)
 # legend(150,0.8, paste(c('Kaplan-Meier', 'Nelson-Aalen')), lwd=2, col=1:2, bty='o')
 
-############### Grafico
-
 (km1 <- survfit(dados_sob~1))
 (km2 <- survfit(Loco_sob~1))
 (km3 <- survfit(Mtx_sob~1))
@@ -102,36 +103,85 @@ lines(km1, mark.time=T, conf.int=F, lwd=2, col=4)
 legend(130,0.95, paste(c('Sólido localizado', 'Metastático', 'Hematológico',
                          'Completo')), lwd=2, col=1:4, bty='o')
 
-#### 
+# Funcao de risco ---------------------------------------------------------
+require(muhaz)
+
+# Completo
+h0 <- muhaz(dados1$tempo, dados1$status, min.time = 1, max.time = 182)
+
+# Loco
+h1 <- muhaz(Loco$tempo, Loco$status, min.time = 1, max.time = 182)
+
+# Hemato
+h2 <- muhaz(Hemato$tempo, Hemato$status, min.time = 1, max.time = 182)
+
+# Mtx
+h3 <- muhaz(Mtx$tempo, Mtx$status, min.time = 1, max.time = 182)
+
+# Graficos
+plot(h1, mark.time=T, conf.int=F, lwd=2, xlab='Tempo de sobrevida',
+     ylab='Função de Risco Estimada')
+lines(h3, mark.time=T, conf.int=F, lwd=2, col=2)
+lines(h2, mark.time=T, conf.int=F, lwd=2, col=3)
+lines(h0, mark.time=T, conf.int=F, lwd=2, col=4)
+legend(130,0.025, paste(c('Sólido localizado', 'Metastático', 'Hematológico',
+                         'Completo')), lwd=2, col=1:4, bty='o')
 
 
-# Media -------------------------------------------------------------------
+
+
+# Media e Mediana ---------------------------------------------------------
+
 ######### Media dados
-(theta_hat <- 1/(sum(dados$Tempo)/sum(dados$Censura)))
-(tm <- 1/theta_hat)
+theta_hat1 <- 1/(sum(dados1$tempo)/sum(dados1$status))
+tm1 <- 1/theta_hat1
 
 ### Desvio padrao do tempo medio (metodo delta)
-var_tm <- 1/(sum(dados$Censura)*theta_hat^2)
-(sd_tm  <- sqrt(var_tm))
+var_tm1 <- 1/(sum(dados1$status)*theta_hat1^2)
+sd_tm1  <- sqrt(var_tm1)
 
 ### IC para o tempo medio
-(ic_tm <- c(tm - qnorm(0.975)*sd_tm, tm + qnorm(0.975)*sd_tm))
-
+ic_tm1 <- c(tm1 - qnorm(0.975)*sd_tm1, tm1 + qnorm(0.975)*sd_tm1)
 
 ######### Media Loco
+theta_hat2 <- 1/(sum(Loco$tempo)/sum(Loco$status))
+tm2 <- 1/theta_hat2
+
+var_tm2 <- 1/(sum(Loco$status)*theta_hat2^2)
+sd_tm2  <- sqrt(var_tm2)
+
+ic_tm2 <- c(tm2 - qnorm(0.975)*sd_tm2, tm2 + qnorm(0.975)*sd_tm2)
 
 ######### Media Hemato
+theta_hat3 <- 1/(sum(Hemato$tempo)/sum(Hemato$status))
+tm3 <- 1/theta_hat3
+
+var_tm3 <- 1/(sum(Hemato$status)*theta_hat3^2)
+sd_tm3  <- sqrt(var_tm3)
+
+ic_tm3 <- c(tm3 - qnorm(0.975)*sd_tm3, tm3 + qnorm(0.975)*sd_tm3)
 
 ######### Media Mtx
-(theta_hat <- 1/(sum(dados$Tempo)/sum(dados$Censura)))
-(tm <- 1/theta_hat)
+theta_hat4 <- 1/(sum(Mtx$tempo)/sum(Mtx$status))
+tm4 <- 1/theta_hat4
 
-### Desvio padrao do tempo medio (metodo delta)
-var_tm <- 1/(sum(dados$Censura)*theta_hat^2)
-(sd_tm  <- sqrt(var_tm))
+var_tm4 <- 1/(sum(Mtx$status)*theta_hat4^2)
+sd_tm4  <- sqrt(var_tm4)
 
-### IC para o tempo medio
-(ic_tm <- c(tm - qnorm(0.975)*sd_tm, tm + qnorm(0.975)*sd_tm))
+ic_tm4 <- c(tm4 - qnorm(0.975)*sd_tm4, tm4 + qnorm(0.975)*sd_tm4)
+
+print(c('Completo', ic_tm1, 'Sólido localizado', ic_tm2, 
+        'Metastático', ic_tm4, 'Hematológico', ic_tm3))
+
+######### Mediana
+
+km1 # Completo = 62
+km2 # Sólido localizado = NA
+km3 # Metastático = 18
+km4 # Hematológico = 19.5
+
+print(c('Completo', 62, 'Sólido localizado', NA, 
+        'Metastático', 18, 'Hematológico', 19.5))
 
 # Modelos parametricos ----------------------------------------------------
 
